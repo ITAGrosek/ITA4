b5bd17776515e0271ec0128dc1ea59e85400d97d
martingrosek <martin.grosek@student.um.si>
2024-03-28 16:22:46 +0100

    microservice n1 and n3 working on bff-web

--- bff-web/server.js
+++ bff-web/server.js
@@ -22,45 +22,44 @@ app.use(express.json());
 const BOOK_SERVICE_BASE_URL = 'http://localhost:8080/api';
 
 
-  
+// Nastavi osnovni URL mikrostoritve za rezervacije
+const RESERVATION_SERVICE_BASE_URL = 'http://localhost:8081';
+
 
 // Dinamično usmerjanje vseh zahtev pod '/api/*'
 app.use('/api/*', async (req, res) => {
-  // Izgradi ciljni URL s podmenjavo '/api' z osnovnim URL mikrostoritve
-  // Dinamično izgradi ciljni URL tako, da zamenjaš '/api' s praznim nizom,
-  // kar pomeni, da vse zahteve na '/api/something' preusmeriš na 'http://localhost:8080/api/something'
-  const targetUrl = `${BOOK_SERVICE_BASE_URL}${req.originalUrl.replace('/api', '')}`;
-
-  try {
-
-    // Ustvari konfiguracijo za Axios zahtevek, ki vključuje:
-    // - metodo zahteve (GET, POST, PUT, DELETE itd.)
-    // - ciljni URL, na katerega se pošlje zahtevek
-    // - telo zahteve (če je to POST/PUT zahtevek)
-    // - query parametre (če obstajajo)
-    // Konfiguracija Axios zahtevka vključuje metodo, URL, telo (za POST/PUT) in query parametre
-    const axiosConfig = {
-      method: req.method,
-      url: targetUrl,
-      ...(Object.keys(req.body || {}).length > 0 && {data: req.body}),
-      params: req.query,
-    };
-
-    // Posreduj zahtevek do ciljne mikrostoritve in pošlji odgovor nazaj odjemalcu
-    const response = await axios(axiosConfig);
-    res.status(response.status).json(response.data);
-  } catch (error) {
-    // Ustrezno rokovanje z napakami, vključno z napakami mikrostoritve
-    if (error.response) {
-      res.status(error.response.status).send(error.response.data);
-    } else {
-      console.error(error.message);
-      res.sendStatus(500);
+    let targetUrl;
+    if (req.originalUrl.includes('/api/books')) {
+      // Če je zahteva namenjena upravljanju knjig, usmeri na prvo mikrostoritev
+      targetUrl = `${BOOK_SERVICE_BASE_URL}${req.originalUrl.replace('/api', '')}`;
+    } else if (req.originalUrl.includes('/api/reservations')) {
+      // Če je zahteva namenjena upravljanju rezervacij, usmeri na drugo mikrostoritev
+      targetUrl = `${RESERVATION_SERVICE_BASE_URL}${req.originalUrl.replace('/api', '')}`;
     }
-  }
-});
-
-
+  
+    try {
+      // Ustvari konfiguracijo za Axios zahtevek
+      const axiosConfig = {
+        method: req.method,
+        url: targetUrl,
+        ...(Object.keys(req.body || {}).length > 0 && {data: req.body}),
+        params: req.query,
+      };
+  
+      // Pošlji zahtevek na ustrezno končno točko glede na pot
+      const response = await axios(axiosConfig);
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      // Obvladuj napake
+      if (error.response) {
+        res.status(error.response.status).send(error.response.data);
+      } else {
+        console.error(error.message);
+        res.sendStatus(500);
+      }
+    }
+  });
+  
 
 
 
