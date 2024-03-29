b980b77cd0b63989b7ac1d6f5223ab8483b21a80
martingrosek <martin.grosek@student.um.si>
2024-03-29 16:54:33 +0100

    all working

---
+++ bff-mobile/generated/proto/user_grpc_pb.js
@@ -0,0 +1,90 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+'use strict';
+var grpc = require('@grpc/grpc-js');
+var user_pb = require('./user_pb.js');
+
+function serialize_com_feri_bookusers_UserRequest(arg) {
+  if (!(arg instanceof user_pb.UserRequest)) {
+    throw new Error('Expected argument of type com.feri.bookusers.UserRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_com_feri_bookusers_UserRequest(buffer_arg) {
+  return user_pb.UserRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_com_feri_bookusers_UserResponse(arg) {
+  if (!(arg instanceof user_pb.UserResponse)) {
+    throw new Error('Expected argument of type com.feri.bookusers.UserResponse');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_com_feri_bookusers_UserResponse(buffer_arg) {
+  return user_pb.UserResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+// The service definition.
+var UserServiceService = exports.UserServiceService = {
+  // Sends a greeting
+createUser: {
+    path: '/com.feri.bookusers.UserService/CreateUser',
+    requestStream: false,
+    responseStream: false,
+    requestType: user_pb.UserRequest,
+    responseType: user_pb.UserResponse,
+    requestSerialize: serialize_com_feri_bookusers_UserRequest,
+    requestDeserialize: deserialize_com_feri_bookusers_UserRequest,
+    responseSerialize: serialize_com_feri_bookusers_UserResponse,
+    responseDeserialize: deserialize_com_feri_bookusers_UserResponse,
+  },
+  getUser: {
+    path: '/com.feri.bookusers.UserService/GetUser',
+    requestStream: false,
+    responseStream: false,
+    requestType: user_pb.UserRequest,
+    responseType: user_pb.UserResponse,
+    requestSerialize: serialize_com_feri_bookusers_UserRequest,
+    requestDeserialize: deserialize_com_feri_bookusers_UserRequest,
+    responseSerialize: serialize_com_feri_bookusers_UserResponse,
+    responseDeserialize: deserialize_com_feri_bookusers_UserResponse,
+  },
+  updateUser: {
+    path: '/com.feri.bookusers.UserService/UpdateUser',
+    requestStream: false,
+    responseStream: false,
+    requestType: user_pb.UserRequest,
+    responseType: user_pb.UserResponse,
+    requestSerialize: serialize_com_feri_bookusers_UserRequest,
+    requestDeserialize: deserialize_com_feri_bookusers_UserRequest,
+    responseSerialize: serialize_com_feri_bookusers_UserResponse,
+    responseDeserialize: deserialize_com_feri_bookusers_UserResponse,
+  },
+  deleteUser: {
+    path: '/com.feri.bookusers.UserService/DeleteUser',
+    requestStream: false,
+    responseStream: false,
+    requestType: user_pb.UserRequest,
+    responseType: user_pb.UserResponse,
+    requestSerialize: serialize_com_feri_bookusers_UserRequest,
+    requestDeserialize: deserialize_com_feri_bookusers_UserRequest,
+    responseSerialize: serialize_com_feri_bookusers_UserResponse,
+    responseDeserialize: deserialize_com_feri_bookusers_UserResponse,
+  },
+  listUsers: {
+    path: '/com.feri.bookusers.UserService/ListUsers',
+    requestStream: false,
+    responseStream: true,
+    requestType: user_pb.UserRequest,
+    responseType: user_pb.UserResponse,
+    requestSerialize: serialize_com_feri_bookusers_UserRequest,
+    requestDeserialize: deserialize_com_feri_bookusers_UserRequest,
+    responseSerialize: serialize_com_feri_bookusers_UserResponse,
+    responseDeserialize: deserialize_com_feri_bookusers_UserResponse,
+  },
+};
+
+exports.UserServiceClient = grpc.makeGenericClientConstructor(UserServiceService);
--- bff-mobile/server.js
+++ bff-mobile/server.js
@@ -2,7 +2,84 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = 4000; // Uporabite drugačen port za BFF-Mobile, npr. 4000
+
+
 const grpc = require('@grpc/grpc-js');
+const { UserServiceClient } = require('./generated/proto/user_grpc_pb');
+const { UserRequest } = require('./generated/proto/user_pb');
+
+
+
+const client = new UserServiceClient(
+  'localhost:9002',
+  grpc.credentials.createInsecure()
+);
+
+// Middleware za branje JSON telesa
+app.use(express.json());
+
+app.post('/api/users', (req, res) => {
+  // Ustvari Protobuf sporočilo
+  const request = new UserRequest();
+  request.setName(req.body.name);
+  request.setSurname(req.body.surname);
+  request.setAge(req.body.age);
+  request.setEmail(req.body.email);
+  request.setUsername(req.body.username);
+  
+  // Izvedi gRPC klic
+  client.createUser(request, (error, response) => {
+    if (error) {
+      console.error('gRPC error:', error);
+      res.status(500).send("An error occurred: " + error.message);
+    } else {
+      // `response` je Protobuf objekt, pretvoriti ga morate v JSON
+      res.json(response.toObject());
+    }
+  });
+});
+
+app.get('/api/users/:id', (req, res) => {
+  const request = new UserRequest();
+  request.setId(req.params.id);
+
+  client.getUser(request, (error, response) => {
+    if (error) {
+      console.error('gRPC error:', error);
+      res.status(500).send("An error occurred: " + error.message);
+    } else {
+      res.json(response.toObject());
+    }
+  });
+});
+
+
+
+
+
+app.get('/api/users', (req, res) => {
+  const request = new UserRequest();
+
+  const call = client.listUsers(request);
+  const users = [];
+
+  call.on('data', (user) => {
+    users.push(user.toObject());
+  });
+
+  call.on('end', () => {
+    res.json(users);
+  });
+
+  call.on('error', (error) => {
+    console.error('gRPC error:', error);
+    res.status(500).send("An error occurred: " + error.message);
+  });
+
+  call.on('status', (status) => {
+    // lahko tudi obravnavate gRPC status
+  });
+});
 
 app.use(express.json());
 
@@ -12,31 +89,26 @@ const BOOK_SERVICE_BASE_URL = 'http://localhost:8080/api';
 // Nastavi osnovni URL mikrostoritve za rezervacije
 const RESERVATION_SERVICE_BASE_URL = 'http://localhost:8081';
 
-// Dinamično usmerjanje samo GET zahtev pod '/api/*'
-app.get('/api/*', async (req, res) => {
+// Dinamično usmerjanje zahtev (GET in POST) pod '/api/*'
+app.use('/api/*', async (req, res) => {
   let targetUrl;
+
+  // Določi ciljni URL glede na zahtevo
   if (req.originalUrl.includes('/api/books')) {
-    // Če je zahteva namenjena upravljanju knjig, usmeri na prvo mikrostoritev
     targetUrl = `${BOOK_SERVICE_BASE_URL}${req.originalUrl.replace('/api', '')}`;
   } else if (req.originalUrl.includes('/api/reservations')) {
-    // Če je zahteva namenjena upravljanju rezervacij, usmeri na drugo mikrostoritev
     targetUrl = `${RESERVATION_SERVICE_BASE_URL}${req.originalUrl.replace('/api', '')}`;
   }
 
   try {
-    // Samo za GET zahtevke
-    if (req.method !== 'GET') {
-      throw new Error('Only GET requests are allowed.');
-    }
-
-    // Ustvari konfiguracijo za Axios GET zahtevek
     const axiosConfig = {
-      method: 'GET',
+      method: req.method,
       url: targetUrl,
+      ...(req.method === 'POST' && {data: req.body}),
       params: req.query,
     };
 
-    // Pošlji GET zahtevek na ustrezno končno točko glede na pot
+    // Pošlji zahtevek na ustrezno končno točko glede na pot in metodo
     const response = await axios(axiosConfig);
     res.status(response.status).json(response.data);
   } catch (error) {
@@ -49,6 +121,9 @@ app.get('/api/*', async (req, res) => {
     }
   }
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`BFF-Mobile Web listening at http://localhost:${PORT}`);
